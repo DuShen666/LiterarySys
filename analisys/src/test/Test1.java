ec153a37b36e5f710d77d3bb0c0c81ce70856edf
DuShen666 <981040863@qq.com>
2017-04-17 12:26:27 +0800

    测试1

---
+++ analisys/src/test/Test1.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.Test;
+
+import ccut.Analisys;
+
+public class Test1 {
+
+	@Test
+	public void test() {
+		System.out.println(Analisys.AnalisysString("asdfghasfh"));
+	}
+	
+	
+}
