884b0afe368f7d8987e0a157b65cbf18547a6dd1
DuShen666 <981040863@qq.com>
2017-04-17 12:35:41 +0800

    测试5

---
+++ analisys/src/test/Test5.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.Test;
+
+import ccut.Analisys;
+
+public class Test5 {
+	@Test
+	public void test() {
+		System.out.println(Analisys.AnalisysString("qazwsxedcrfqaz"));
+	}
+}
