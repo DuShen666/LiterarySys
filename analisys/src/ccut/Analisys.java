3dd534475f314e623b10e0a42b30c881b17e12d7
DuShen666 <981040863@qq.com>
2017-04-17 12:10:04 +0800

    分析方法

--- analisys/src/ccut/Analisys.java
+++ analisys/src/ccut/Analisys.java
@@ -1,5 +1,29 @@
 package ccut;
 
-public class Analisys {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Analisys {
+	public static int AnalisysString(String string) {
+		int length = string.length();
+		if (length > 30000) {
+			return -1;
+		}
+		Map<Character, Integer> cursor = new HashMap<Character, Integer>();
+		cursor.put(string.charAt(0), 0);
+		int[] lengthAt = new int[string.length()];
+		lengthAt[0] = 1;
+		int max = 0;
+		for (int i = 1; i < length; i++) {
+			char c = string.charAt(i);
+			if (cursor.containsKey(c)) {
+				lengthAt[i] = Math.min(lengthAt[i - 1] + 1, i - cursor.get(c));
+			} else {
+				lengthAt[i] = lengthAt[i - 1] + 1;
+			}
+			max = Math.max(max, lengthAt[i]);
+			cursor.put(c, i);
+		}
+		return max;
+	}
 }
